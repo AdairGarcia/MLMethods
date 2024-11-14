5ae1ff2dc2bcfcc7b07a2316a204ade08a617c9c
AdairGarcia <agarciat1800@alumno.ipn.mx>
2024-11-13 22:19:29 -0600

    Detalles test

--- test.py
+++ test.py
@@ -8,10 +8,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
-from sklearn.utils import compute_class_weight
-import numpy as np
 
 from methods import *
+
 
 def get_x_y(mode):
     if mode == 'raw':
@@ -71,43 +70,36 @@ def test():
 
         for text_representation_name, text_representation in text_representations.items():
             for model_name, model in models.items():
-                if model_name == 'Naive Bayes' and text_representation_name == 'TF-IDF':
-                    print(f'-------------------------{text_representation_name} - {model_name}-------------------------{mode.upper()}')
-                    pipelines(x_train, y_train, x_test, y_test, text_representation, model, 1)
-                    print()
-
-                print(f'-------------------------{text_representation_name} - {model_name}-------------------------{mode.upper()}')
-                pipelines(x_train, y_train, x_test, y_test, text_representation, model)
-                print()
-                # pipeline = Pipeline(
-                #     [('vectorizer', text_representation), ('classifier', model)]
-                # )
-                #
-                # param_grid = param_grids[model_name]
-                # param_grid.update({
-                #     'vectorizer__max_features': [5000],
-                #     'vectorizer__ngram_range': [(1, 1)],
-                #     'vectorizer__max_df': [0.75, 0.85, 0.95],
-                #     'vectorizer__min_df': [1, 2, 5]
-                # })
-                #
-                # # Compute class weights
-                # class_weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
-                # class_weight_dict = {i: class_weights[i] for i in range(len(class_weights))}
-                #
-                # # Update the classifier with class weights
-                # pipeline.set_params(classifier__class_weight=class_weight_dict)
-                #
-                # grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='f1_macro', n_jobs=-1, verbose=1)
-                # grid_search.fit(x_train, y_train)
+                # if model_name == 'Naive Bayes' and text_representation_name == 'TF-IDF':
+                #     print(f'-------------------------{text_representation_name} - {model_name}-------------------------{mode.upper()}')
+                #     pipelines(x_train, y_train, x_test, y_test, text_representation, model, 1)
+                #     print()
                 #
                 # print(f'-------------------------{text_representation_name} - {model_name}-------------------------{mode.upper()}')
-                # print(f'Mejores parametros {text_representation_name} - {model_name}: {grid_search.best_params_}')
-                # print(f'Mejor macro avg f1-score: {grid_search.best_score_}')
-                #
-                # y_pred = grid_search.predict(x_test)
-                # print(classification_report(y_test, y_pred, zero_division=0))
+                # pipelines(x_train, y_train, x_test, y_test, text_representation, model)
                 # print()
+                pipeline = Pipeline(
+                    [('vectorizer', text_representation), ('classifier', model)]
+                )
+
+                param_grid = param_grids[model_name]
+                param_grid.update({
+                    'vectorizer__ngram_range': [(1, 1)],
+                    'vectorizer__max_df': [0.75, 0.85, 0.95],
+                    'vectorizer__min_df': [1, 2, 5]
+                })
+
+                # Crear el grid search
+                grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='f1_macro', n_jobs=-1, verbose=1)
+                grid_search.fit(x_train, y_train)
+
+                print(f'-------------------------{text_representation_name} - {model_name}-------------------------{mode.upper()}')
+                print(f'Mejores parametros {text_representation_name} - {model_name}: {grid_search.best_params_}')
+                print(f'Mejor macro avg f1-score: {grid_search.best_score_}')
+
+                y_pred = grid_search.predict(x_test)
+                print(classification_report(y_test, y_pred, zero_division=0))
+                print()
             print()
     end_time = time.time()
     print(f'Tiempo de ejecución: {end_time - start_time} segundos')
